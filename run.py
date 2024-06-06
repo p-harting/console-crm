e59b04550acffdf7f9174f78ad38562faf5369ab
Pascal Harting <pascalharting98@gmail.com>
2024-06-06 14:23:42 +0000

    Add main class and function structure to run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from data_manager import DataManager
-import time
 
 # Initialize the data manager
 creds_file = 'creds.json'
@@ -8,24 +7,17 @@ worksheet_name = 'contacts'
 
 data_manager = DataManager(creds_file, spreadsheet_name, worksheet_name)
 
-# Get all data
-data = data_manager.get_all_data()
-print(data)
+class CRM:
+    def __init__(self):
+        data = data_manager.get_all_data()
+        print(data)
 
-# Get row by row number
-row_data = data_manager.get_row(2)
-print(row_data)
+    def run(self):
+        print('hello world')
 
-# Get row by id
-row_data = data_manager.get_row_by_id(1)
-print(row_data)
+def main():
+    app = CRM()
+    app.run()
 
-# Append data to end
-append_data = ['Example', 'Example', '2000-01-01', 'example@example.com', '000-000-0000', 'Example', 'Example', 'Example']
-data_manager.append_row(append_data)
-
-# Delete row by row number
-data_manager.delete_row(2)
-
-# Delete row by id
-data_manager.delete_row_by_id(1)
+if __name__ == "__main__":
+    main()
