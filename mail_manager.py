56b896c2a7c0eb04b03214dfa1390793940bee69
Pascal Harting <pascalharting98@gmail.com>
2024-06-14 09:21:17 +0000

    Make mail_manager.py pep8 conform and add function comments

--- mail_manager.py
+++ mail_manager.py
@@ -1,7 +1,16 @@
 import smtplib
 
+
 class MailManager:
     def send_mail(self, receiver_mail, subject, message, password):
+        """
+        Sends an email using SMTP protocol to the specified receiver address.
+
+        :param receiver_mail: The email address of the recipient.
+        :param subject: The subject line of the email.
+        :param message: The body content of the email.
+        :param password: The password for the sender's email account.
+        """
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         text = f"Subject: {subject}\n\n{message}"
